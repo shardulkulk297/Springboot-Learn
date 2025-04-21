af6ffd0df02d6b5fddd246f77b50fcb0babbc10b
Shardul <shardulkulk297@gmail.com>
2025-04-21 23:38:04 +0530

    Completed Hellow world Project using springboot

---
+++ demo/src/main/java/com/example/learn/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.learn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+
+}
