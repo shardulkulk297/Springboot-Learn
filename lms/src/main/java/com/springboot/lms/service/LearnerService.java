3df8bf873931f86670b2a91a02354c0b5eb80eee
Shardul <shardulkulk297@gmail.com>
2025-05-26 14:48:34 +0530

    Created api's for other crud operations

--- lms/src/main/java/com/springboot/lms/service/LearnerService.java
+++ lms/src/main/java/com/springboot/lms/service/LearnerService.java
@@ -4,6 +4,8 @@ import com.springboot.lms.model.Learner;
 import com.springboot.lms.repository.LearnerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LearnerService {
 
@@ -16,6 +18,37 @@ public class LearnerService {
     public Learner insertLearner(Learner learner){
         return learnerRepository.save(learner);
     }
+
+    public List<Learner> getAllLearners(){
+        return learnerRepository.findAll();
+    }
+
+    public void deleteLearnerById(int id){
+        learnerRepository.deleteById(id);
+    }
+
+    public Learner getLearnerById(int id){
+        return learnerRepository.findById(id).orElseThrow(()->new RuntimeException("Learner Not found"));
+    }
+
+    public Learner updateLearnerById(int id, Learner learner){
+        Learner learnerToUpdate = learnerRepository.findById(id).orElseThrow(()->new RuntimeException("Learner Not found"));
+
+        if(learner.getName() != null){
+            learnerToUpdate.setName(learner.getName());
+        }
+        if(learner.getContact()!= null){
+            learnerToUpdate.setContact(learner.getContact());
+        }
+        if(learner.getEmail() != null){
+            learnerToUpdate.setContact(learner.getContact());
+        }
+
+        return learnerRepository.save(learnerToUpdate);
+
+     }
+
+
 
 
 
